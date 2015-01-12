7a39a9f3b3c17409eed6bf5a7545f55d9af6c5be
skatsuta <katsuta_soshi@cyberagent.co.jp>
2015-01-12 21:47:16 +0900

    [add] add a test for main

---
+++ src/api/main_test.go
@@ -0,0 +1,21 @@
+package api
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"testing"
+)
+
+func TestApi(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Api Suite")
+}
+
+var _ = Describe("main", func() {
+	It("add 1 + 1 = 2", func() {
+		expect := 2
+		actual := Add(1, 1)
+
+		Expect(actual).To(Equal(expect))
+	})
+})
